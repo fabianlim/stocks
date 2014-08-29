7cffce60f130b688470e8f3849f58a3e6ec21949
fabianlim <fabian.lim@gmail.com>
2014-08-29 03:05:31 -0700

    update the cron timing

--- ticker/cron.py
+++ ticker/cron.py
@@ -4,7 +4,7 @@ from ticker.models import Ticker, Quote
 
 from populate_ticker import pull_sg_quotes
 class PullQuotes(CronJobBase):
-    RUN_EVERY_MINS = 1 # every 10 mins
+    RUN_EVERY_MINS = 30 # every 10 mins
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'visual.pull_query'    # a unique code
